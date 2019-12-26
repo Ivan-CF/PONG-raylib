6591935881c09aa76a9c28ed189fdf96b23e59e3
Ivan-CF <58510990+Ivan-CF@users.noreply.github.com>
2019-12-26 21:03:49 +0100

    Todos los sonidos añadidos

--- Final Pong.c
+++ Final Pong.c
@@ -47,23 +47,23 @@ int main()
     
     Rectangle player;
     
-    player.width = 20;
-    player.height = 100;    
+    player.width = 10;
+    player.height = 60;    
     player.x = 50;
     player.y = screenHeight/2 - player.height/2;
     
     Rectangle enemy;
     
-    enemy.width = 20;
-    enemy.height = 2;    
+    enemy.width = 10;
+    enemy.height = 60;    
     enemy.x = screenWidth - 50 - enemy.width;
     enemy.y = screenHeight/2 - enemy.height/2;
     
     //int ballRadius;
-    int maxVelocity = 8;
-    int minVelocity = 8;
+    int maxVelocity = 10;
+    int minVelocity = 3.5;
     
-    int ballSize = 15;
+    int ballSize = 10;
     
     Vector2 ball;
     
@@ -75,17 +75,9 @@ int main()
     ballVelocity.x = minVelocity;
     ballVelocity.y = minVelocity;
     
-    int enemySpeed = 1;
-    
-
-    //int ballRadius;
-
-    
-    //
+    int enemySpeed = 5;
 
 
-
-    
     Rectangle backRect = { 30, 25, 100, 15} ; //Background Rectangle
     int margin = 2; 
     Rectangle fillRect = { backRect.x + margin, backRect.y + margin, backRect.width - (2 * margin), backRect.height - (2 * margin)};
@@ -107,12 +99,7 @@ int main()
     bool lose = false;
     
 
-    
-    int drainLife = 1;
-    int healLife = 3;
-    
-    //
-    
+  
     bool pause = false;
     
 
@@ -126,11 +113,36 @@ int main()
     InitWindow(screenWidth, screenHeight, windowTitle);
     
     // NOTE: If using textures, declare Texture2D variables here (after InitWindow)
+    Texture2D title = LoadTexture("resources/cielo.png");
+    Texture2D background = LoadTexture("resources/forest.png");
+    
+    float scrollingBack = 0.0f;
     // NOTE: If using SpriteFonts, declare SpriteFont variables here (after InitWindow)
     
     // NOTE: If using sound or music, InitAudioDevice() and load Sound variables here (after InitAudioDevice)
     InitAudioDevice();
+    float timePlayed = 0.0f;
     
+    Music music = LoadMusicStream("resources/title.ogg");
+    PlayMusicStream(music);
+    
+    Music palaiz = LoadMusicStream("resources/yess.ogg");
+    PlayMusicStream(palaiz);
+    
+    Music palade = LoadMusicStream("resources/ooh.ogg");
+    PlayMusicStream(palade);
+    
+    Sound cuandomarcas = LoadSound("resources/cuandomarcas.ogg");
+            
+    Sound cuandotemarcan = LoadSound("resources/cuandotemarcan.ogg");
+    
+    Music cuandoganas = LoadMusicStream("resources/cuandoganas.ogg");
+    PlayMusicStream(cuandoganas);
+    
+    Music cuandopierdes = LoadMusicStream("resources/cuandopierdes.ogg");
+    PlayMusicStream(cuandopierdes);
+
+    //Sound palaiz = LoadSound("resources/yes.wav");
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
     
@@ -181,6 +193,11 @@ int main()
             {
                 // Update TITLE screen data here!
                 
+                UpdateMusicStream(music);
+                PlayMusicStream(music);
+                
+                scrollingBack -= 0.3f;
+                if (scrollingBack <= -title.width*2) scrollingBack = 0;
                 // TODO: Title animation logic.......................(0.5p)
                 if(fadeOut2)
             {
@@ -201,7 +218,10 @@ int main()
             }
                         
             // TODO: "PRESS ENTER" logic.........................(0.5p)
-            if (IsKeyPressed(KEY_ENTER)) screen = GAMEPLAY;
+            if (IsKeyPressed(KEY_ENTER)){
+                StopMusicStream(music);
+                screen = GAMEPLAY;
+               } 
                
                framesCounter++;
                 
@@ -209,30 +229,33 @@ int main()
             case GAMEPLAY:
             { 
                 // Update GAMEPLAY screen data here!
-
+                scrollingBack -= 0.3f;
+                if (scrollingBack <= -background.width*2) scrollingBack = 0;
                 // TODO: Ball movement logic.........................(0.2p)
                 
-                if(ball.x > screenWidth - ballSize){
+                if(ball.x > screenWidth +150){
                     //Marca la pala izquierda
-                    //PlaySound(fxGoal);
-                    //player++;
                     ball.x = screenWidth/2;
                     ball.y = screenHeight/2;
                     ballVelocity.x = -minVelocity;
                     ballVelocity.y = minVelocity;
                     lifeRect2.width -= 20;
-                    
-                    
-                   
-                }else if(ball.x < ballSize){
+                }
+                if (ball.x > 800 && ball.x < 820){
+                    PlaySound(cuandomarcas);
+                }
+                  
+                else if(ball.x < ballSize -290){
                     //Marca la pala derecha
-                    //PlaySound(fxGoal);
                     //enemy++;
                     ball.x = screenWidth/2;
                     ball.y = screenHeight/2;
                     ballVelocity.x = minVelocity;
                     ballVelocity.y = minVelocity;
                     lifeRect.width -= 20;
+                }
+                if (ball.x <= 0 && ball.x >= -10){
+                    PlaySound(cuandotemarcan);
                 }
                
                  if((ball.y > screenHeight - ballSize) || (ball.y < ballSize) ){
@@ -270,14 +293,18 @@ int main()
                 }
                 
                 if(CheckCollisionCircleRec(ball, ballSize, player)){
+                    UpdateMusicStream(palaiz);
+                    PlayMusicStream(palaiz);
                 if(ballVelocity.x<0){
-                        //PlaySound(fxWav);
                     if(abs(ballVelocity.x)<maxVelocity){                    
-                            ballVelocity.x *=-1.5;
-                            ballVelocity.y *= 1.5;
+                            ballVelocity.x *=-1.1;
+                            ballVelocity.y *= 1.1;
                         }else{
                             ballVelocity.x *=-1;
                         }
+                    UpdateMusicStream(palaiz);
+                    PlayMusicStream(palaiz);
+                    enemySpeed = 4;
                     }
                 }                 
                 
@@ -291,14 +318,19 @@ int main()
                 }
                 
                     if(CheckCollisionCircleRec(ball, ballSize, enemy)){
+                        UpdateMusicStream(palade);
+                        PlayMusicStream(palade);
                     if(ballVelocity.x>0){
                         //PlaySound(fxWav);
                         if(abs(ballVelocity.x)<maxVelocity){                    
-                            ballVelocity.x *=-1.5;
-                            ballVelocity.y *= 1.5;
+                            ballVelocity.x *=-1.1;
+                            ballVelocity.y *= 1.1;
                         }else{
                             ballVelocity.x *=-1;
                         }
+                    UpdateMusicStream(palade);
+                    PlayMusicStream(palade);
+                    enemySpeed = 4;
                     }
                 }
                 
@@ -329,8 +361,14 @@ int main()
                 // Update END screen data here!
                 
                 // TODO: Replay / Exit game logic....................(0.5p)
-
-                
+            if (lifeRect2.width <= 0){
+                UpdateMusicStream(cuandoganas);
+                PlayMusicStream(cuandoganas);
+            }
+            if (lifeRect.width <= 0){
+                UpdateMusicStream(cuandopierdes);
+                PlayMusicStream(cuandopierdes);
+            }   
             if (IsKeyPressed(KEY_R)){
                screen = GAMEPLAY;
                lifeRect.width = 96;
@@ -355,29 +393,32 @@ int main()
             {
                 case LOGO: 
                 {
+                    ClearBackground(BLACK);
                     // Draw LOGO screen here!
                     
                     // TODO: Draw Logo...............................(0.2p)
-                    DrawText("Programmed with 'Raylib' by Ivan Carrizosa",25,200,35, Fade(BROWN, alpha));
+                    DrawText("Programmed with 'Raylib' by Ivan Carrizosa",25,200,35, Fade(SKYBLUE, alpha));
                 
                     
                 } break;
                 case TITLE: 
                 {
                     // Draw TITLE screen here!
-                    
+                    DrawTextureEx(title, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+                    DrawTextureEx(title, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
                     // TODO: Draw Title..............................(0.2p)
-                    DrawText("FINAL PONG", 100, 100, 100,Fade(BEIGE, alpha2));
+                    DrawText("SKY PONG", 150, 100, 100,Fade(DARKGREEN, alpha2));
                    
                     
                     // TODO: Draw "PRESS ENTER" message..............(0.2p)
-                    if ((framesCounter/30)%2) DrawText("PRESS ENTER", 300, 300, 30, BLUE);
+                    if ((framesCounter/30)%2) DrawText("PRESS ENTER", 300, 300, 30, DARKBLUE);
                     
                 } break;
                 case GAMEPLAY:
                 { 
                     // Draw GAMEPLAY screen here!
-                    DrawLine(iaLinex, 0, iaLinex , screenHeight, BLACK);
+                    DrawTextureEx(background, (Vector2){ scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+                    DrawTextureEx(background, (Vector2){ background.width*2 + scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
                     // TODO: Draw player and enemy...................(0.2p)
                     DrawRectangleRec(player, BLUE);                              
                     DrawRectangleRec(enemy, RED);
@@ -402,15 +443,15 @@ int main()
                 case ENDING: 
                 {
                     // Draw END screen here!
-                    
+                    ClearBackground(BLACK);
                     // TODO: Draw ending message (win or loose)......(0.2p)
                     if(lifeRect.width <= 0){
-                        DrawText("WINNER IS ENEMY", screenWidth/2 + 100, screenHeight/2 + 20, 30, RED);
-                        DrawText("ERES UNA MIERDA", screenWidth/2 - 350, screenHeight/2 + 20, 30, BLUE);
+                        DrawText("ENEMY WIN", screenWidth/2 + 100, screenHeight/2 - 30, 30, RED);
+                        DrawText("LOOSER!", screenWidth/2 - 250, screenHeight/2 - 30, 30, BLUE);
                     }
                     if(lifeRect2.width <= 0){
-                        DrawText("WINNER IS PLAYER 1", screenWidth/2 - 350, screenHeight/2 + 20, 30, BLUE);
-                        DrawText("VA OTRA, CAGAO!", screenWidth/2 + 50, screenHeight/2 + 20, 30, RED);
+                        DrawText("YOU WIN", screenWidth/2 - 250, screenHeight/2 - 30, 30, BLUE);
+                        DrawText("ENEMY DEFEATED", screenWidth/2 + 50, screenHeight/2 - 30, 30, RED);
                     }
                     if (secondsCounter < 0){
                         DrawText("FIN DEL TIEMPO, ES UN EMPATE", screenWidth/2 - 100, screenHeight/2, 30, ORANGE);
@@ -436,7 +477,16 @@ int main()
     //--------------------------------------------------------------------------------------
     
     // NOTE: Unload any Texture2D or SpriteFont loaded here 
-  
+    UnloadTexture(background);
+    UnloadTexture(title);
+    UnloadMusicStream(music);
+    UnloadMusicStream(palaiz);
+    UnloadMusicStream(palade);
+    UnloadSound(cuandomarcas);
+    UnloadSound(cuandotemarcan);
+    UnloadMusicStream(cuandoganas);
+    UnloadMusicStream(cuandopierdes);
+    CloseAudioDevice();
     
     CloseWindow();        
     //--------------------------------------------------------------------------------------
